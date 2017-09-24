36f44a41afc836185b3b3b1109155104914a7081
Georgii Lebeda <lebeda.georgiy@gmail.com>
2017-09-24 08:51:34 +0300

    Finished with more data types. Override toSting()

--- src/com/plualsight/calcengine/CalculateHelper.java
+++ src/com/plualsight/calcengine/CalculateHelper.java
@@ -1,6 +1,11 @@
 package com.plualsight.calcengine;
 
 public class CalculateHelper {
+    private static final char ADD_SYMBOL = '+';
+    private static final char SUBTRACT_SYMBOL = '-';
+    private static final char MULTIPLY_SYMBOL = '*';
+    private static final char DIVIDE_SYMBOL = '/';
+
     MathCommand command;
     double leftValue;
     double rightValue;
@@ -20,6 +25,7 @@ public class CalculateHelper {
                 break;
             case Subtract:
                 calculator = new Subtracter(leftValue, rightValue);
+                break;
             case Divide:
                 calculator = new Divider(leftValue, rightValue);
                 break;
@@ -39,5 +45,35 @@ public class CalculateHelper {
             command = MathCommand.Subtract;
         else if(commandString.equalsIgnoreCase(MathCommand.Multiply.toString()))
             command = MathCommand.Multiply;
+    }
+
+    @Override
+    public String toString() {
+        char symbol = ' ';
+        switch (command) {
+            case Add:
+                symbol = ADD_SYMBOL;
+                break;
+            case Subtract:
+                symbol = SUBTRACT_SYMBOL;
+                break;
+            case Multiply:
+                symbol = MULTIPLY_SYMBOL;
+                break;
+            case Divide:
+                symbol = DIVIDE_SYMBOL;
+                break;
+        }
+
+        StringBuilder sb = new StringBuilder(20);
+        sb.append(leftValue);
+        sb.append(' ');
+        sb.append(symbol);
+        sb.append(' ');
+        sb.append(rightValue);
+        sb.append(" = ");
+        sb.append(result);
+
+        return sb.toString();
     }
 }
